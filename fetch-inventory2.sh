bfa8b67f4e26b1ccfc43200018e623ca59f6589e
BenjaminVitas <45081378+BenjaminVitas@users.noreply.github.com>
2022-09-26 18:03:50 -0400

    Initial Commit

---
+++ fetch-inventory2.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+rm inventory2.json
+IFS=', ' read -r -a array < dealers30k-40k.txt
+
+for i in ${array[@]}
+do
+  curl \
+  --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+      "brand": "TOY",
+      "mode": "content",
+      "group": true,
+      "groupmode": "full",
+      "relevancy": false,
+      "pagesize": 300,
+      "pagestart": 0,
+      "filter": {
+          "year": [2021, 2022, 2023], 
+          "series": ["tacoma", "tundra", "rav4", "4runner", "camry", "corolla"], 
+          "dealers": ["'$i'"],
+          "andfields": ["accessory", "packages", "dealer"]
+        }
+    }' \
+  https://www.toyota.com/config/services/inventory/search/getInventory \
+  | jq -c \
+    '.body.response.docs
+      | .[] 
+      | {
+          dealer: "'$i'",
+          vin: .vin,
+          year: .year.code,
+          vehicle: .grade.series_code,
+          model: .grade.code,
+          enginge: .engine.title,
+          transmission: .transmission.title,
+          drivetrain: .drive.title,
+          cab: .cab.title,
+          bed: .bed.title,
+          color: .exteriorcolor.title,
+          interior: .interiorcolor.title,
+          base_msrp: .priceInfo.baseMSRP,
+          total_msrp: .priceInfo.totalMSRP,
+          availability_date: .availabilityDate,
+          total_packages: .accessories | length,
+          packages: .accessories | map(.title) | join(", "),
+          created_at: now | strflocaltime("%Y-%m-%d %H:%M:%S")
+        }' \
+  >> inventory2.jsonl
+  echo $i
+done
+
+cat inventory2.jsonl | jq -s '.' > inventory2.json
+rm inventory2.jsonl
