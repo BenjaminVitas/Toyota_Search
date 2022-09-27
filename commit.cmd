fcf00901eb8215a6987c66a43a9d44f6aa68ca28
Benjamin Vitas <benjaminvitas@gmail.com>
2022-09-27 09:07:33 -0400

    $(read -p 'Commit description: ')

--- commit.cmd
+++ commit.cmd
@@ -1,5 +1,5 @@
-bash fetch-inventory.sh && \
-git add . && \
-git add -u && \
-git commit -m "$(read -p 'Commit description: ')" && \
+bash fetch-inventory.sh
+git add .
+git add -u
+git commit -m "$(read -p 'Commit description: ')"
 git push origin HEAD
